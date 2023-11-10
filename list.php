5ea26be812bae334f1bd6aa2b860e2b3332204a1
lubalsaneli <lucibalsaneli@hotmail.com>
2023-11-10 18:10:43 -0300

    luciana att adocao-details

--- list.php
+++ list.php
@@ -2,14 +2,14 @@
 
 include_once "conexao.php";
 
-$query_animais = "SELECT id, nome, idade, porte FROM animais";
+$query_animais = "SELECT * FROM animais";
 $result_animais = $pdo->prepare($query_animais);
 $result_animais->execute();
 
 $dados = "";
 while($row_animal = $result_animais->fetch(PDO::FETCH_ASSOC)) {
     extract($row_animal);
-    $dados .= "<a href=adocao-details.php><div class=felpudo-card id=teste><img src=><span>$nome</span></div></a>";
+    $dados .= "<a href=adocao-details.php?id=$ID><div class=felpudo-card id=teste><img src=$imagem><span>$nome</span></div></a>";
 }
 
 echo $dados;
