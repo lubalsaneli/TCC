6d36dd4298bbc9df9d630e2d801e99e01b056fd3
lubalsaneli <lucibalsaneli@hotmail.com>
2023-11-12 02:06:40 -0300

    luciana - ultimas att

--- logado.php
+++ logado.php
@@ -1,7 +1,3 @@
-<?php
-    include_once "conexao.php";  
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
@@ -12,13 +8,13 @@
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;700;900&display=swap" rel="stylesheet">      
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,1000&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="./css/header-style.css">
-        <link rel="stylesheet" href="./css/inicio-style.css">
+        <link rel="stylesheet" href="./css/logado-style.css">
         <link rel="stylesheet" href="./css/footer-style.css">
-        <link rel="stylesheet" href="./css/adocao-style.css">
         <link rel="shortcut icon" type="imagex/svg" href="./img/felupudos-shotcut.svg">
         <title>Felpudos | Encontre um amigo, salve uma vida</title>
     </head>
-<body>
+    <body>
+        
     <header>
         <nav class="nav-header">
             <div class="logo">
@@ -69,44 +65,66 @@
         </div>
 
     </header>
-    <main>
 
-            <div class="container_total_felpudos">
-                <span id="total_felpudos"></span>
-            </div>
 
-            <div class="title-adocao">
-                <h2>Encontre seu Felpudo</h2>
-                <p>Aqui você encontra todos os nossos felpudos que estão disponíveis para adoção, basta clicar e agendar sua visita.</p>
-            </div>
-            
-            <!-- ESTLIZAÇÃO DOS CARDS -->
-            <section class="felpudos-container">
-            
-            </section>
-        </main>
-       
 
-        <footer>
-            <div class="footer-content">
-                <nav class="footer-nav-shortcut">
-                    <h3>Mapa do Site</h3>
-                    <ul>
-                        <li><a href="./inicio.html">Início</a></li>
-                        <li><a href="./quem-somos.html">Quem Somos</a></li>
-                        <li><a href="./contato.html">Contato</a></li>
-                        <li><a href="./quero-ajudar.html">Quero Ajudar</a></li>
-                    </ul>
-                </nav>
+        <main>
 
-                <nav>
-                    <a href="inicio.html">
-                        <img src="./img/logo-icon-white.svg" alt="Logo Felpudos">
+            <section class="tela-logado-content">
+                <div class="div1">
+                    <h2 border-top>Bem vindo(a),</h2>  
+                    <p>neste espaço, você vai conseguir adotar seu felpudo e ter maior facilidade no processo de agendamento de consultas.</p> 
+                </div>
+                    
+
+                <div class="div2">
+                    <a href="./adocao.php" class="id-link">
+                        <div class="card-container">
+                            ADOTAR UM FELPUDO
+                        </div>
                     </a>
-                </nav>
-        </footer>
 
-        <script src="./js/custom.js"></script>
+                    <a href="./agendar-consulta.php" class="id-link">
+                        <div class="card-container">
+                            AGENDAR UMA CONSULTA
+                        </div>
+                    </a>
+                </div>
+                    
+
+            </section>
+
+
+
+
+
+
+
+        </main>
+
+
+
+            <footer>
+                <div class="footer-content">
+                    <nav class="footer-nav-shortcut">
+                        <h3>Mapa do Site</h3>
+                        <ul>
+                            <li><a href="./inicio.html">Início</a></li>
+                            <li><a href="./quem-somos.html">Quem Somos</a></li>
+                            <li><a href="./contato.html">Contato</a></li>
+                            <li><a href="./quero-ajudar.html">Quero Ajudar</a></li>
+                        </ul>
+                    </nav>
+    
+                    <nav>
+                        <a href="inicio.html">
+                            <img src="./img/logo-icon-white.svg" alt="Logo Felpudos">
+                        </a>
+                    </nav>
+                </footer>
+            </footer>
+        </div>
+
         
         <script src="./js/header-script.js"></script>
     </body>
